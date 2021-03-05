c6d7315000841df6176035315d0d7ded769d08a0
luohao10001 <55565872+luohao10001@users.noreply.github.com>
2021-03-05 21:31:06 +0800

    Update rewrite.js

--- rewrite.js
+++ rewrite.js
@@ -1,1 +1,3 @@
 https://vip.75787.com/app/index.php https://raw.githubusercontent.com/age174/-/main/xsmm.js
+https:\/\/qmyd\.yichengw\.cn\/* https://raw.githubusercontent.com/ziye11/JavaScript/main/Task/qmyd.js
+https:\/\/yuedongzu\.yichengw\.cn\/* https://raw.githubusercontent.com/ziye11/JavaScript/main/Task/yuedongzu.js
